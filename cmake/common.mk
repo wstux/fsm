f880fc644cad2316e03421cf5b8ea1973d625ce7
Chistyakov Alexander <wstux1@gmail.com>
2022-03-08 09:46:36 +0300

    + cmake: Add basic build system;

---
+++ cmake/common.mk
@@ -0,0 +1,22 @@
+export NPROC	?= $(shell (nproc))
+export NJOB	?= $(shell expr '(' $(NPROC) + 1 ')')
+
+define common_rule
+
+build_$(1)/Makefile:
+	@mkdir -p $$(@D) && cd $$(@D) && cmake -DCMAKE_BUILD_TYPE="$(1)" ../
+
+$(1)/%: build_$(1)/Makefile
+	@make -j $(NJOB) --output-sync=target --no-print-directory -C build_$(1) $$*
+
+$(1)/clean:
+	@rm -rf build_$(1)
+
+$(1)/all_ut_run: build_$(1)/Makefile
+	@make -j $(NJOB) -C build_$(1) test
+
+endef
+
+$(eval $(call common_rule,debug))
+$(eval $(call common_rule,release))
+
