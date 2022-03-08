54ac4991f504492e9c6e20f45aca22713bbae27e
Chistyakov Alexander <wstux1@gmail.com>
2022-03-08 19:03:02 +0300

    + libs,tests: Add trie library and ut;

---
+++ libs/fsm/trie.h
@@ -0,0 +1,136 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2022 Chistyakov Alexander.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef FSM_TRIE_H
+#define FSM_TRIE_H
+
+#include <map>
+#include <vector>
+
+#include "fsm/fsm.h"
+
+namespace fsm {
+
+template<typename TValue, typename TTrans, template<typename> class TStateCont = std::vector,
+         typename TValueCont = std::map<typename TTrans::state_type, TValue>>
+class trie
+{
+    using fsm_type = fsm<TTrans, TStateCont>;
+    using trie_type = trie<TValue, TTrans, TStateCont, TValueCont>;
+    using value_list = TValueCont;
+
+public:
+    using event_type = typename fsm_type::event_type;
+    using ptr = std::shared_ptr<trie_type>;
+    using state_id = typename fsm_type::state_id;
+    using pointer_type = const TValue* const;
+    using value_type = TValue;
+
+    trie()
+        : m_fsm()
+    {}
+
+    explicit trie(const size_t reserve_size)
+        : m_fsm(reserve_size)
+    {}
+
+    const state_id& begin() const { return m_fsm.begin(); }
+
+    void clear() { m_fsm.clear(); }
+
+    state_id follow(state_id& st, const event_type& ev) const { return m_fsm.follow(st, ev); }
+
+    bool follow(state_id& st, const event_type& ev, value_type& val) const
+    {
+        st = m_fsm.follow(st, ev);
+        if (st == invalid()) {
+            return false;
+        }
+        val = value(st);
+        return true;
+    }
+
+    template<template<typename> class TCont>
+    bool follow(const TCont<event_type>& cnt) const { return m_fsm.follow(cnt); }
+
+    template<template<typename> class TCont>
+    bool follow(const TCont<event_type>& cnt, value_type& val) const
+    {
+        state_id st = begin();
+        for (const event_type& ev : cnt) {
+            st = follow(st, ev);
+            if (st == invalid()) {
+                return false;
+            }
+        }
+        if (is_available(st)) {
+            val = value(st);
+            return true;
+        }
+        return false;
+    }
+
+    template<template<typename> class TCont>
+    bool insert(const TCont<event_type>& cnt, const value_type& val)
+    {
+        state_id st = begin();
+        for (size_t i = 0; i < cnt.size(); ++i) {
+            const event_type& ev = cnt[i];
+            state_id st_to = follow(st, ev);
+            if (st_to == invalid()) {
+                st_to = insert(st, ev, false);
+            }
+            st = st_to;
+        }
+        m_fsm.make_available(st);
+        m_values[st] = val;
+        return true;
+    }
+
+    state_id insert(const state_id& from, const event_type& ev, const bool is_available = false)
+    {
+        return m_fsm.insert(from, ev, is_available);
+    }
+
+    const state_id& invalid() const { return m_fsm.invalid(); }
+
+    bool is_available(const state_id& st) const { return m_fsm.is_available(st); }
+
+    void reserve(const size_t size) const { m_fsm.reserve(size); }
+
+    void set_value(const state_id& st, const value_type& val) { m_values[st] = val; }
+
+    size_t size() const { return m_fsm.size(); }
+
+    const value_type& value(const state_id& st) const { return m_values.at(st); }
+
+private:
+    fsm_type m_fsm;
+    value_list m_values;
+};
+
+} // namespace fsm
+
+#endif // FSM_TRIE_H
+
--- tests/ut_fsm.cpp
+++ tests/ut_fsm.cpp
@@ -1,9 +1,11 @@
 #include <array>
 #include <deque>
+#include <limits>
 #include <map>
 #include <set>
 
 #include "fsm/fsm.h"
+#include "fsm/trie.h"
 
 #include "testdefs.h"
 
@@ -16,57 +18,101 @@ using str_trans_flex = fsm::trans_traits<char, uint32_t, std::map<char, uint32_t
 
 INIT_TYPE_TESTS(fsm, str_trans_flat, str_trans_flex)
 
-TYPED_TEST(fsm, base)
+TYPED_TEST(fsm, base_fsm)
 {
     using str_trans = TType;
-    using str_fsm = fsm::fsm<str_trans, std::deque>;
+    using str_fsm = fsm::fsm<str_trans>;
+
+    const std::vector<std::string> etalon = {"abcd", "abce", "apple", "banana", "banan"};
 
     str_fsm fsm;
     EXPECTED(fsm.size() == 2);
-
-    EXPECTED(fsm.insert(std::string("abcd")));
-    EXPECTED(fsm.insert(std::string("abce")));
-    EXPECTED(fsm.insert(std::string("apple")));
-    EXPECTED(fsm.insert(std::string("banana")));
-    EXPECTED(fsm.insert(std::string("banan")));
-
-    EXPECTED(! fsm.follow(std::string("a")));
-    EXPECTED(! fsm.follow(std::string("ab")));
-    EXPECTED(! fsm.follow(std::string("abc")));
-    EXPECTED(fsm.follow(std::string("abcd")));
-    EXPECTED(fsm.follow(std::string("abce")));
-    EXPECTED(fsm.follow(std::string("apple")));
-    EXPECTED(fsm.follow(std::string("banana")));
-    EXPECTED(fsm.follow(std::string("banan")));
-}
-
-/*TEST(fsm, chech_invalid)
-{
-    str_fsm fsm;
-    EXPECTED(fsm.size() == 2);
-
-    EXPECTED(fsm.insert(std::string("abcd")));
-    EXPECTED(fsm.insert(std::string("abce")));
-    EXPECTED(fsm.insert(std::string("apple")));
-    EXPECTED(fsm.insert(std::string("banana")));
-    EXPECTED(fsm.insert(std::string("banan")));
-
-    std::string prefix(1, 1);
-    for (size_t i = 0; i < 6; ++i) {
-        
-        for (char ch = 1; ch < 127; ++ch) {
-        }
+    for (const std::string& str : etalon) {
+        EXPECTED(fsm.insert(str)) << str << std::endl;
     }
 
     EXPECTED(! fsm.follow(std::string("a")));
     EXPECTED(! fsm.follow(std::string("ab")));
     EXPECTED(! fsm.follow(std::string("abc")));
-    EXPECTED(fsm.follow(std::string("abcd")));
-    EXPECTED(fsm.follow(std::string("abce")));
-    EXPECTED(fsm.follow(std::string("apple")));
-    EXPECTED(fsm.follow(std::string("banana")));
-    EXPECTED(fsm.follow(std::string("banan")));
-}*/
+
+    for (const std::string& str : etalon) {
+        EXPECTED(fsm.follow(str)) << str << std::endl;
+    }
+}
+
+TYPED_TEST(fsm, base_trie)
+{
+    using str_trans = TType;
+    using str_trie = fsm::trie<size_t, str_trans>;
+
+    const std::vector<std::string> etalon = {"abcd", "abce", "apple", "banana", "banan"};
+
+    str_trie trie;
+    EXPECTED(trie.size() == 2);
+    for (size_t i = 0; i < etalon.size(); ++i) {
+        const std::string& str = etalon[i];
+        EXPECTED(trie.insert(str, i)) << str << std::endl;
+    }
+
+    EXPECTED(! trie.follow(std::string("a")));
+    EXPECTED(! trie.follow(std::string("ab")));
+    EXPECTED(! trie.follow(std::string("abc")));
+
+    for (size_t i = 0; i < etalon.size(); ++i) {
+        const std::string& str = etalon[i];
+
+        size_t val;
+        EXPECTED(trie.follow(str, val)) << str << std::endl;
+        EXPECTED(val == i) << val << " != " << i << std::endl;
+    }
+}
+
+TYPED_TEST(fsm, DISABLED_chech_invalid)
+{
+    using str_trans = TType;
+    using str_fsm = fsm::fsm<str_trans, std::deque>;
+
+    const std::set<std::string> etalon = {"abcd", "abc", "app", "ban", "bana"};
+
+    str_fsm fsm;
+    EXPECTED(fsm.size() == 2);
+
+    for (const std::string& str : etalon) {
+        EXPECTED(fsm.insert(str)) << str << std::endl;
+    }
+
+    const size_t k_ch_count = 7;
+    const size_t k_max_ch = 127;
+    size_t counter = 0;
+    for (size_t i = 0; i < std::numeric_limits<uint32_t>::max(); ++i) {
+        size_t remainder = i;
+        std::string str;
+
+        bool is_failed = false;
+        while (remainder > 0) {
+            const char ch = static_cast<char>(remainder % k_max_ch);
+            if ((ch < 1) || (ch > 126)) {
+                is_failed = true;
+                break;
+            }
+            remainder = remainder >> k_ch_count;
+            str.insert(str.cbegin(), ch);
+        }
+        if (is_failed) {
+            continue;
+        }
+        if (etalon.count(str) != 0) {
+            continue;
+        }
+        ++counter;
+        EXPECTED(! fsm.follow(str)) << str << std::endl;
+    }
+
+    for (const std::string& str : etalon) {
+        EXPECTED(fsm.follow(str)) << str << std::endl;
+    }
+    //EXPECTED(counter == ) << counter << std::endl;
+}
 
 int main()
 {
